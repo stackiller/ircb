61e584cc21f697c8552df41d1d47793efe26e87d
aSynKronus <david.vegasc@gmail.com>
2019-05-29 21:59:46 -0300

    some changes in the readMsg function

--- b0t.c
+++ b0t.c
@@ -24,12 +24,11 @@
 #include "ssl.c"
 
 /* main - create SSL context and connect */
-int main(int count, char *args[]) {
-  int sockfd; // socket file descriptor
-  char ircMsg[MSG_LEN]; // ircMsg messages
-
+int main(int count, char *args[])
+{
   // check arguments number
-  if(count != 6) {
+  if(count != 6)
+  {
     printf("usage: %s <server_addr> <port> <nick> <password> \"#channel1,#channel2,#channel3,#channel4,#channel5\"\n", args[0]);
     exit(0);
   }
@@ -42,30 +41,34 @@ int main(int count, char *args[]) {
   irc.chans = args[5]; // copy into irc.chans
   irc.ctx = initCtx(); // initialize SSL context
   irc.ssl = SSL_new(irc.ctx); // create new SSL connection state
-  sockfd = newCon(irc.host, atoi(irc.port)); // create connection socket
+  irc.sockfd = newCon(irc.host, atoi(irc.port)); // create connection socket
 
   // attach socket descriptor
-  SSL_set_fd(irc.ssl, sockfd);
-  if(SSL_connect(irc.ssl) == FAIL) {
+  SSL_set_fd(irc.ssl, irc.sockfd);
+  if(SSL_connect(irc.ssl) == FAIL)
     ERR_print_errors_fp(stderr);
-  }
-
+  else
   // show SSL chiper - certificates
   printf(
     "[%s%s*%s] Connected with %s encryption\n",
     fgGreen, blink, resetCl, SSL_get_cipher(irc.ssl)
   ); showCerts(irc.ssl);
 
-  setNick(); // identify user
-  setCreds(); // set credentials
+  setNick(irc.nick); // identify user
+  setCreds(irc.pass); // set credentials
 
   // receive messages
-  do {
-    readMsg(ircMsg); // read for messages
-  } while(1);
+  do
+  {
+    irc.buffer = NULL; // point pointer to null
+    irc.buffer = readMsg(irc.buffer); // store the read message
+    printf("%s", irc.buffer); // print message
+    free(irc.buffer); // release buffer
+  }
+  while(1);
 
   SSL_free(irc.ssl); // release connection state
-  close(sockfd); // close sockfd
+  close(irc.sockfd); // close sockfd
   SSL_CTX_free(irc.ctx); // release context
   return(0);
 }
