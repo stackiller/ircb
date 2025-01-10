2a4143d1db2833b81a5a939e441a62f0c9010f0b
stackiller <david.vegasc@gmail.com>
2025-01-10 02:25:42 -0300

    nothing else

--- ircb.c
+++ ircb.c
@@ -18,60 +18,55 @@
 
 #include "prototypes.h"
 
-#include "modules/manage.c" // gerenciamento
-#include "modules/moderate.c" // moderação do canal
+#include "modules/core.c" // core funcs of bot.
+#include "modules/sys.c" // system functions.
 
 #include "all_funcs.c"
 #include "irc_funcs.c"
 #include "ssl_funcs.c"
 
-/* main - sem muito o que dizer xP */
+/* Main xP */
 int
 main(int n_args, char *s_args[]) {
   
-  // checa o número de argumentos.
+  // check the number of arguments.
   if(n_args < 6) {
     usage(s_args[0]);
     exit(0);
   }
 
-  // Atribui os respectivos dados aos campos.
+  // assign the respective data to the fields.
   irc.host = s_args[1];
   irc.port = s_args[2];
   irc.nick = s_args[3];
   irc.pass = s_args[4];
   irc.chans = s_args[5];
 
-  // Arte do cabeçalho.
+  // header art.
   bot_Header();
 
   irc.ctx = init_Ctx();
   irc.ssl = SSL_new(irc.ctx);
   irc.sockfd = new_Conn(irc.host, atoi(irc.port));
 
-  bot_Nick(irc.nick); // identifica o usuário.
-  bot_Creds(irc.nick, irc.pass); // define as credenciais.
+  bot_Nick(irc.nick); // identifies the user.
+  bot_Creds(irc.nick, irc.pass); // sets credentials.
 
-  // recebe as mensagens
+  // receive the messages
   do {
-    irc.buffer = r_Buffer(); // lê o buffer.
+    irc.buffer = r_Buffer(); // reads the buffer.
 
     char *endOf = strstr(irc.buffer, ":End of message of the day.");
 
     if(endOf != NULL) {
-      // bot_Nick(irc.nick); // identifica o usuário.
-      bot_Creds(irc.nick, irc.pass); // define as credenciais.
-      bot_Join(irc.chans); // entra para o canal.
+      bot_Join(irc.chans); // join to channels.
     }
 
-    fprintf(stdout, "%s", irc.buffer); // exibe a entrada do buffer.
-    
-    if(irc.buffer != NULL) {
-      free(irc.buffer);
-    }
+    fprintf(stdout, "%s", irc.buffer); // show the received messages.
+    release(irc.buffer);
   } while(1);
 
-  // libera o estado da conexão.
+  // releases the connection state.
   SSL_free(irc.ssl);
   SSL_CTX_free(irc.ctx);
   close(irc.sockfd);
