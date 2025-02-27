e98e039f36e7e9515ddf2ee5976d39378189b203
stackiller <david.vegasc@gmail.com>
2025-02-27 03:32:15 -0300

    inclusion of prototypes of new functions.

--- prototypes.h
+++ prototypes.h
@@ -39,32 +39,43 @@ char bot_Brand[] = {
 /* get the size of the array pointer. */
 #define ARRAY_SIZE(x) ((sizeof(x)) / (sizeof(x[0])))
 
-/* IRC data structure */
+/* Irc connection data structure. */
 typedef struct {
   SSL_CTX *ctx; // ssl context pointer.
   SSL *ssl; // ssl descriptor.
   int sockfd; // socket descriptor
   char *host, *port; // host & port.
   char *nick, *pass; // nick & pass.
-  char *chans, *buffer; // channels & buffer.
+  char *chans, **buffer_matrix; // channels & buffer.
+  int buffer_matrix_size;
 } irc_d;
 
 /* Function prototypes */
 SSL_CTX* init_Ctx(void); // initializes a new ssl context.
 void show_Certs(SSL*); // show ssl certificates.
+
 int new_conn(const char*, int); // creates a socket.
 int init_conn(irc_d *irc); // init connection.
 int end_conn(irc_d *irc); // end connection.
 
+int checkNull(void*); // checks if the pointer is null.
 int str_Cmp(char*, char*); // compares two strings.
-int diff(char, char*); // compare if ch is different from the elements of the chs array.
+int diff_from_chs(char, char*); // compare if ch is different from the elements of the chs array.
 int matrix_haveNull(char**, int); // checks if an array has any null elements.
-void matrix_Destroy(char **, int); // destroys a matrix.
-void release(void *ptr); // releases a pointer.
-char *realoca(char*, int); // reallocates an array to a new size.
 
-char* r_Buffer(void); // reads the buffer.
-void msg_Send(char*); // send a message to the irc server.
+char *string(char*); // initialize a new string.
+char *str_realloc(char*, int); // reallocates an array to a new size.
+
+void null_safe_release(void *ptr); // releases a pointer with null safety.
+
+void matrix_Destroy(char **, int); // destroys a matrix.
+void read_matrix_Buffer(char**, int); // Reads the matrix buffer.
+
+char** matrix_Alloc(int); // allocates matrix buffer.
+char** matrix_Realloc(char**, int); // reallocates the matrix size.
+
+char** format_Buffer(char*, int*);
+char** r_Buffer(int*); // reads the buffer.
 
 char* get_Dst(char*); // gets the recipient.
 char* get_Msg(char*); // get the message.
@@ -72,24 +83,30 @@ char* get_Src(char*); // get the source.
 char* get_nArg(char*, int); // get n arg.
 char* get_Args(char*); // get all arguments from message.
 
-int bot_Exec(char*, char*, char*); // controls the commands to be executed by the bot.
+int get_Code(char*); // gets the code of server message.
 
+int bot_Exec(char*, char*, char*); // controls the commands to be executed by the bot.
 int bot_Pong(char*); // pong.
+
+void msg_Send(char*); // send a message to the irc server.
+
 void bot_Nick(char*); // nick.
 void bot_Creds(char*, char*); // credentials.
 void bot_Join(char*); // join.
 void bot_Priv(char*, char*); // privmsg.
 void bot_Part(char*); // part.
-
 void bot_Shell(char*, char*); // executes shell commands.
 
 void bot_Header(void); // bot header.
 void usage(char*); // helps user.
 
 // Sizes.
-#define BUFFER_SIZE 512 * 10
-#define BUFFER_LINE_SIZE 512
+#define BUFFER_MAX_SIZE 512 * 10
+#define BUFFER_MAX_LINE_SIZE 512
 #define BOT_MAX_LEN 50
+
+// Messages.
+#define PART_MESSAGE "bye :P"
 
 // Erros.
 #define FAIL -1
