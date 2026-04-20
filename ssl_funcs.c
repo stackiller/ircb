ac76f2e5d2be53d3deaa5c10d4a2972cd7642db5
stackiller <david.vegasc@gmail.com>
2026-04-20 00:07:41 -0300

    fixed invalid memory access

--- ssl_funcs.c
+++ ssl_funcs.c
@@ -1,11 +1,11 @@
 /* Creates a new socket connection. */
-int
-new_conn(const char *hostname, const char *port)
-{
+#include <sys/socket.h>
+int new_conn(const char *hostname, const char *port) {
   int sockfd, s;
   struct addrinfo *res, *next, hints;
-  res = addri;
-  next = addri_next;
+
+  struct sockaddr_storage addr_copy;
+  socklen_t addrlen_copy;
 
   // hints for addrinfo.
   memset(&hints, 0, sizeof(hints));
@@ -20,27 +20,35 @@ new_conn(const char *hostname, const char *port)
 
   // get address of hostname.
   s = getaddrinfo(hostname, port, &hints, &res);
-  if(s < 0) {
+  if (s != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
     return -1;
   }
 
+  sockfd = -1;
+
   // create ipv4 socket.
-  for(next = res; next != NULL; next = next->ai_next) {
+  for (next = res; next != NULL; next = next->ai_next) {
     sockfd = socket(next->ai_family, next->ai_socktype, next->ai_protocol);
-    if(sockfd > 0) {
+    if (sockfd > 0) {
+      memcpy(&addr_copy, next->ai_addr, next->ai_addrlen);
+      addrlen_copy = next->ai_addrlen;
       break;
     }
   }
+
   freeaddrinfo(res);
+  res = NULL;
+  next = NULL;
 
   // exit if socket fails.
-  if(sockfd < 0) {
+  if (sockfd < 0) {
     perror("new_conn");
     return -1;
   }
 
   // connects to the IRC server.
-  if((connect(sockfd, next->ai_addr, next->ai_addrlen)) != 0) {
+  if ((connect(sockfd, (struct sockaddr *)&addr_copy, addrlen_copy)) != 0) {
     printf("(%s*%s%s) Invalid address: %s\n", fYellow, fBlink, fRs, hostname);
     close(sockfd);
     return -1;
@@ -50,90 +58,78 @@ new_conn(const char *hostname, const char *port)
   SSL_set_fd(irc.ssl, sockfd);
 
   // initializes the ssl connection to the IRC server.
-  if(SSL_connect(irc.ssl) == 0)
-  {
+  if (SSL_connect(irc.ssl) == 0) {
     close(sockfd);
     ERR_print_errors_fp(stderr);
     return -1;
-  }
-  else
-  {
-    printf(
-      "[%s%s*%s] Connected with %s.\n",
-      fGreen, fBlink, fRs, SSL_get_cipher(irc.ssl));
+  } else {
+    printf("[%s%s*%s] Connected with %s.\n", fGreen, fBlink, fRs,
+           SSL_get_cipher(irc.ssl));
   }
   show_Certs(irc.ssl); // show certificates.
-  
+
   return sockfd;
 }
 
 /* Initialize the SSL engine */
-SSL_CTX*
-init_Ctx(void)
-{
+SSL_CTX *init_Ctx(void) {
   const SSL_METHOD *method; // SSL method pointer
-  SSL_CTX *ctx; // SSL context pointer
- 
+  SSL_CTX *ctx;             // SSL context pointer
+
   // create new method and context
-  method = TLS_method(); // create new client-method instance
+  method = TLS_method();     // create new client-method instance
   ctx = SSL_CTX_new(method); // create new SSL context
-  if(ctx == NULL)
-  {
+  if (ctx == NULL) {
     ERR_print_errors_fp(stderr);
     exit(1);
-  } return(ctx);  // return SSL context
+  }
+  return (ctx); // return SSL context
 }
 
 /* Show certificates */
-void
-show_Certs(SSL *ssl)
-{
+void show_Certs(SSL *ssl) {
   // X509 certificate and line pointer
   X509 *cert; // X509 certificate pointer
   char *line; // line buffer
 
   // get the server's certificate
   cert = SSL_get_peer_certificate(ssl);
-  if(cert != NULL)
-  {
+  if (cert != NULL) {
     printf("Server certificate:\n"); // show message before.
     line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0); // get subject.
 
     printf("|_ Subject: %s\n", line); // show subject.
-    free(line); // free malloc'ed string
+    free(line);                       // free malloc'ed string
 
     // get issuer name
-    line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0); // get the issuer.
+    line =
+        X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0); // get the issuer.
     printf("|_ Issuer: %s\n", line); // show issuer certificate.
-    
-    free(line); // free the malloc'ed string
+
+    free(line);      // free the malloc'ed string
     X509_free(cert); // free the malloc'ed certificate copy
-   }
-   else
+  } else
     printf("[%s%s?%s] No certificates.\n", fYellow, fBlink, fRs);
 }
 
 /* Init connection */;
-int init_conn(irc_d *irc)
-{
+int init_conn(irc_d *irc) {
   irc->ctx = init_Ctx();
   irc->ssl = SSL_new(irc->ctx);
-  
+
   /* keep trying to create a new connection until successul. */
-  while((irc->sockfd = new_conn(irc->host, irc->port)) < 0) {
+  while ((irc->sockfd = new_conn(irc->host, irc->port)) < 0) {
     sleep(10);
   }
 
-  for(int i=0; i < 3; i++)
+  for (int i = 0; i < 3; i++)
     bot_Nick(irc->nick); // identifies the user.
 
   return 0;
 }
 
 /* End connection */
-int
-end_conn(irc_d *irc)
-{
+int end_conn(irc_d *irc) {
   global_variables_reset();
   SSL_shutdown(irc->ssl);
   SSL_free(irc->ssl);
@@ -143,9 +139,7 @@ end_conn(irc_d *irc)
 }
 
 /* End connection */
-int
-recon_conn(irc_d *irc)
-{
+int recon_conn(irc_d *irc) {
   end_conn(irc);
   init_conn(irc);
   irc->reconnect = 0;
